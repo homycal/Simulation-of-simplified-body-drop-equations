bd98d2bf01ac2d72a4423bdc0bd85deb204eaf23
Jérémy HOLAY <21800092@etu.unicaen.fr>
2020-05-04 20:42:49 +0200

    Ungraduated axes added

--- Simulation_equations/MainWindow.xaml.cs
+++ Simulation_equations/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const float CANVAS_PADDING = 20;
         public MainWindow()
         {
             InitializeComponent();
@@ -44,13 +45,14 @@ namespace View
         private void plotEquation(Equation equation)
         {
             canvasMainGraph.Children.Clear();
+            drawAxes(canvasMainGraph);
             SolidColorBrush redBrush = new SolidColorBrush();
             redBrush.Color = Colors.Red;
             //float precision = equation.getZeroHeight()
             float precision = 1;
             LinkedList<Model.Point> points = equation.getPoints(precision);
-            float scaleX = (float)canvasMainGraph.RenderSize.Width / points.Count;
-            float scaleZ = (float)canvasMainGraph.RenderSize.Height / points.Count;
+            float scaleX = (float)(canvasMainGraph.RenderSize.Width - CANVAS_PADDING)/ points.Count;
+            float scaleZ = (float)(canvasMainGraph.RenderSize.Height - CANVAS_PADDING) / points.Count;
             Model.Point latest = null;
             foreach(Model.Point point in points)
             {
@@ -67,14 +69,41 @@ namespace View
         {
             double invert = canvasMainGraph.RenderSize.Height;
             Line l = new Line();
-            l.X1 = scaleX * p1.X;
-            l.X2 = scaleX * p2.X;
-            l.Y1 = invert - scaleZ * p1.Z;
-            l.Y2 = invert - scaleZ * p2.Z;
+            l.X1 = (scaleX * p1.X)+CANVAS_PADDING;
+            l.X2 = (scaleX * p2.X)+CANVAS_PADDING;
+            l.Y1 = invert - CANVAS_PADDING - scaleZ * p1.Z;
+            l.Y2 = invert - CANVAS_PADDING - scaleZ * p2.Z;
             l.StrokeThickness = 1;
             l.Stroke = brush;
 
             canvasMainGraph.Children.Add(l);
+        }
+
+        private void drawAxes(Canvas canvas)
+        {
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+
+            //X-Axis
+            Line xAxis = new Line();
+            xAxis.X1 = CANVAS_PADDING;
+            xAxis.X2 = CANVAS_PADDING;
+            xAxis.Y1 = 0;
+            xAxis.Y2 = canvas.RenderSize.Height;
+            xAxis.StrokeThickness = 1;
+            xAxis.Stroke = blackBrush;
+            canvasMainGraph.Children.Add(xAxis);
+
+            //Y-Axis
+            Line yAxis = new Line();
+            yAxis.X1 = 0;
+            yAxis.X2 = canvas.RenderSize.Width;
+            yAxis.Y1 = canvas.RenderSize.Height - CANVAS_PADDING;
+            yAxis.Y2 = canvas.RenderSize.Height - CANVAS_PADDING;
+            yAxis.StrokeThickness = 1;
+            yAxis.Stroke = blackBrush;
+            canvasMainGraph.Children.Add(yAxis);
         }
     }
 }
