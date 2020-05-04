db2271c35f76992e7b763b49806780c10f2e266f
Jérémy HOLAY <21800092@etu.unicaen.fr>
2020-05-04 07:52:25 +0200

    Equation class

--- Simulation_equations/Simulation_equations/Program.cs
+++ Simulation_equations/Simulation_equations/Program.cs
@@ -7,6 +7,15 @@ namespace Simulation_equations
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("a : ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("b : ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("c : ");
+            float c = float.Parse(Console.ReadLine());
+
+            Equation equation = new Equation(a, b, c);
+            Console.WriteLine(equation);
         }
     }
 }
